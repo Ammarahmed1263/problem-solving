77f9928604c1a37a660b8bf2462404aa6f1c622e
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-01-23 19:06:00 +0200

    add C.Sale.cpp for calculating the sum of negative prices

---
+++ 2-array-manipulation/C.Sale.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> prices[i];
+    }
+
+    sort(prices.begin(), prices.end());
+
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (prices[i] >= 0) {
+            break;
+        }
+        sum += prices[i];
+    }
+    
+    cout << abs(sum);
+
+    return 0;
+}
